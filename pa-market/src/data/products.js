63818e4177cf59ca2a9dd4b3cc35b498a3bed3a7
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-05-06 16:23:17 -0300

    Add products data.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

---
+++ pa-market/src/data/products.js
@@ -0,0 +1,92 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Morango',
+    price: 10,
+    image: '1.png',
+  },
+  {
+    id: 2,
+    name: 'Abacate',
+    price: 15,
+    image: '2.png',
+  },
+  {
+    id: 3,
+    name: 'Carne bovina',
+    price: 50,
+    image: '3.png',
+  },
+  {
+    id: 4,
+    name: 'Frango',
+    price: 30,
+    image: '4.png',
+  },
+  {
+    id: 5,
+    name: 'Coca-cola',
+    price: 7,
+    image: '5.png',
+  },
+  {
+    id: 6,
+    name: 'Peixe',
+    price: 40,
+    image: '6.png',
+  },
+  {
+    id: 7,
+    name: 'Macarrão',
+    price: 12,
+    image: '7.png',
+  },
+  {
+    id: 8,
+    name: 'Ovos',
+    price: 8,
+    image: '8.png',
+  },
+  {
+    id: 9,
+    name: 'Manteiga de amendoim',
+    price: 22,
+    image: '9.png',
+  },
+  {
+    id: 10,
+    name: 'Leite',
+    price: 9,
+    image: '10.png',
+  },
+  {
+    id: 11,
+    name: 'Nutella',
+    price: 20,
+    image: '11.png',
+  },
+  {
+    id: 12,
+    name: 'Kitkat',
+    price: 5,
+    image: '12.png',
+  },
+  {
+    id: 13,
+    name: 'Açaí',
+    price: 25,
+    image: '13.png',
+  },
+  {
+    id: 14,
+    name: 'Pão',
+    price: 8,
+    image: '14.png',
+  },
+  {
+    id: 15,
+    name: 'Maçã',
+    price: 1,
+    image: '15.png',
+  },
+];
