c0dd5d6ab0437d32da412e4fb435baf23da6a7cf
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-05-07 19:52:16 -0300

    Add new route.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

--- pa-market/src/Routes.js
+++ pa-market/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 import ProductsResult from './pages/ProductsResult';
 
 const Routes = () => (
@@ -10,6 +11,7 @@ const Routes = () => (
       <Route path="/" exact component={Home} />
       <Route path="/products" exact component={Products} />
       <Route path="/products/result" component={ProductsResult} />
+      <Route path="/products/:id" component={ProductDetail} />
     </Switch>
   </BrowserRouter>
 );
